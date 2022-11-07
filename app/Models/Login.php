02032d7e9e66df81e83e940508a1af5659992d95
20jz0114 <105410289+20jz0114@users.noreply.github.com>
2022-11-07 13:43:39 +0900

    .

--- app/Models/Login.php
+++ app/Models/Login.php
@@ -5,14 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use App\Models\ViewLogin;
 
 class Login extends Model
 {
     use HasFactory;
 
-    public static function getPwdById(){S
+    public static function getPwdById(){
 
-        return $pwd = DB::select('Select pwd From LOGIN_VIEW where pwd = `12345`');
+        return ViewLogin::all();
     }
 
 }
